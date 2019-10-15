0a5c6a786a2255b48db41d8520f0a1b135e84618
Sean Brown <sbrown@bamboohr.com>
2019-10-14 21:59:10 -0600

    comments and attachments oh my..

--- src/rojo-mail/components/views/single-view/single-view.js
+++ src/rojo-mail/components/views/single-view/single-view.js
@@ -1,5 +1,6 @@
 
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import MaterialIcon from 'material-icons-react';
 
@@ -12,38 +13,108 @@ import './single-view.css';
 
 export function SingleView(props) {
 
+	const [comments, setComments] = useState([]);
+	
+	useEffect(() => {
+		getComments();
+	}, []);
+
+	const getComments = () => {
+		const { post } = props;
+
+		if (!post) {
+			return;
+		}
+
+		const commentsUrl = `${ post.data.url }.json`;
+
+		axios
+			.get(commentsUrl)
+			.then((response) => {
+				setComments(response.data[1].data.children);
+			})
+			.catch((error) => {
+				console.log("er", error);
+			});
+	};
+
+	const renderComments = () => {
+		if (!comments || comments.length === 0) {
+			return;
+		}
+
+		const commentList = comments.map((comment) => {
+			const { data } = comment;
+
+			return (
+				<div className="SingleViewCommment" key={ comment.id }>
+					<div className="SingleViewContent__Row">
+						<div className="SingleViewContent__author">
+							{ data.author }
+							<span className="SingleViewContent__subreddit">{ subredditString }</span>
+							<div className="SingleViewContent__toMe">to me</div>
+						</div>
+					</div>
+
+					<div className="SingleViewContent__content">
+						{ data.body }
+					</div>
+				</div>
+			);
+		});
+			
+		return (
+			<div className="SingleViewCommments">
+				{ commentList }
+			</div>
+		);
+	};
+
 	const { onBack, post } = props;
+	const { data } = post;
 
 	if (!post) {
 		return null;
 	}
 
-	const { data } = post;
-
-	console.log('post', post);
-
 	const time = moment.unix(data.created_utc).utc();
 	const timeCreated = time.format('MMM, D YYYY, h:m a');
 	const timeAgo = time.fromNow();
+	const subredditString = `<${ data.subreddit_name_prefixed }>`;
 
 	return (
 		<div className="SingleView">
 			<SingleItemBar onBack={ onBack } />
 
-			<div className="SingleViewContent">
-				<div className="SingleViewContent__title">{ data.title }</div>
-				
-				<div className="SingleViewContent__Row">
-					<div className="SingleViewContent__author">{ data.author }</div>
-					<div className="SingleViewContent__created">{ timeCreated } ({ timeAgo })</div>
+			<div className="SingleViewContainer">
+				<div className="SingleViewColumn">
+					<MaterialIcon icon="account_circle" />
 				</div>
 
-				<div className="SingleViewContent__subreddit">{ data.subreddit_name_prefixed }</div>
-				<div className="SingleViewContent__content">{ data.selftext }</div>
+				<div className="SingleViewContent">
+					<div className="SingleViewContent__title">{ data.title }</div>
+					
+					<div className="SingleViewContent__Row">
+						<div className="SingleViewContent__author">
+							{ data.author }
+							<span className="SingleViewContent__subreddit">{ subredditString }</span>
+							<div className="SingleViewContent__toMe">to me</div>
+						</div>
 
-				<div className="SingleViewContent__content">{ data.selftext_html }</div>
+						<div className="SingleViewContent__created">{ timeCreated } ({ timeAgo })</div>
+					</div>
+
+					<div className="SingleViewContent__content">{ data.selftext }</div>
+
+					<div className="SingleViewContent__content">comments { data.num_comments }</div>
+					<div className="SingleViewContent__content">upvotes { data.score }</div>
+					<div className="SingleViewContent__content"><a href={ data.url } target="_blank">perma link</a></div>
+				</div>
 			</div>
-				
+
+			{/* Turn this into its own component? */}
+			{ renderComments() }
+			
 			<div className="SingleViewFooter">
 				<Button variant="outlined">
 					<MaterialIcon icon="reply" />
