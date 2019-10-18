1033e185cf3a0bf0bb2ddc40b170a2b0c33e5bd5
jbudge3 <jake.budge@me.com>
2019-10-17 20:50:57 -0600

    Updates

---
+++ precache-manifest.f1eed0f97c6446ce2cbd2133e6a6a4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4116cf41a6279b7acb4f4d84a66a08fb",
+    "url": "/rojo-mail/index.html"
+  },
+  {
+    "revision": "521db3f007ce03bc8811",
+    "url": "/rojo-mail/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "67d105cd1fbc66908ce6",
+    "url": "/rojo-mail/static/css/main.ad765bf8.chunk.css"
+  },
+  {
+    "revision": "521db3f007ce03bc8811",
+    "url": "/rojo-mail/static/js/2.01b14e99.chunk.js"
+  },
+  {
+    "revision": "67d105cd1fbc66908ce6",
+    "url": "/rojo-mail/static/js/main.bee67b24.chunk.js"
+  },
+  {
+    "revision": "d5588459654b7aed2756",
+    "url": "/rojo-mail/static/js/runtime~main.d7cf6767.js"
+  }
+]);
