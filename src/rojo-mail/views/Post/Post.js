1f434fa9abd18eb2cd3b9efbad3a6cb9193d95b4
unknown <inphomercial@gmail.com>
2022-06-21 12:19:06 -0600

    extracting out email header sender stuff, and made a component, clean up

--- src/rojo-mail/views/Post/Post.js
+++ src/rojo-mail/views/Post/Post.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
-import moment from 'moment';
-import MaterialIcon from 'material-icons-react';
+// import moment from 'moment';
+// import MaterialIcon from 'material-icons-react';
 
 import { SingleItemBar } from '../../components/SingleItemBar';
 import { PostComments } from '../../components/PostComments';
 import { EmailButton } from '../../components/EmailButton';
+import { EmailSenderHeader } from '../../components/EmailSenderHeader';
 
 import { fetchData } from '../../utils/http';
 
@@ -19,11 +20,6 @@ export function Post (props) {
 		return null;
 	}
 
-	const time = moment.unix(data.created_utc).utc();
-	const timeCreated = time.format('MMM, D YYYY, h:m a');
-	const timeAgo = time.fromNow();
-	const subredditString = `<${data.subreddit_name_prefixed}>`;
-
 	return (
 		<div className='SingleView'>
 			<SingleItemBar onBack={onBack} />
@@ -32,28 +28,7 @@ export function Post (props) {
 				<div className='SingleViewContent'>
 					<div className='SingleViewContent__title'>{data.title}</div>
 
-					<div className='SingleViewContent__Row'>
-						<div className='SingleViewContent__icon'>
-							<MaterialIcon
-								icon='account_circle'
-								size='large'
-								invert={true}
-								color='#CCCCCC'
-							/>
-						</div>
-
-						<div className='SingleViewContent__author'>
-							{data.author}
-							<span className='SingleViewContent__subreddit'>
-								{subredditString}
-							</span>
-							<div className='SingleViewContent__toMe'>to me</div>
-						</div>
-
-						<div className='SingleViewContent__created'>
-							{timeCreated} ({timeAgo})
-						</div>
-					</div>
+					<EmailSenderHeader post={data} />
 
 					{data.media && data.media.reddit_video.fallback_url && (
 						<video controls={true}>
