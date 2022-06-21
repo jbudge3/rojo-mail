d70bc5f63b1c9513329b2f66daa396526a28a3be
unknown <inphomercial@gmail.com>
2022-06-20 23:40:47 -0600

    Fixing buttons, extracted EmailButton, styles

---
+++ src/rojo-mail/components/EmailButton/index.js
@@ -0,0 +1,1 @@
+export { EmailButton } from './EmailButton.js';
--- src/rojo-mail/views/Post/Post.js
+++ src/rojo-mail/views/Post/Post.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import Button from '@material-ui/core/Button';
-import MaterialIcon from 'material-icons-react';
 
 import moment from 'moment';
+import MaterialIcon from 'material-icons-react';
 
 import { SingleItemBar } from '../../components/SingleItemBar';
 import { PostComments } from '../../components/PostComments';
+import { EmailButton } from '../../components/EmailButton';
 
 import { fetchData } from '../../utils/http';
 
@@ -29,14 +29,19 @@ export function Post (props) {
 			<SingleItemBar onBack={onBack} />
 
 			<div className='SingleViewContainer'>
-				<div className='SingleViewColumn'>
-					<MaterialIcon icon='account_circle' />
-				</div>
-
 				<div className='SingleViewContent'>
 					<div className='SingleViewContent__title'>{data.title}</div>
 
 					<div className='SingleViewContent__Row'>
+						<div className='SingleViewContent__icon'>
+							<MaterialIcon
+								icon='account_circle'
+								size='large'
+								invert={true}
+								color='#CCCCCC'
+							/>
+						</div>
+
 						<div className='SingleViewContent__author'>
 							{data.author}
 							<span className='SingleViewContent__subreddit'>
@@ -49,6 +54,17 @@ export function Post (props) {
 							{timeCreated} ({timeAgo})
 						</div>
 					</div>
+
+					{data.media && data.media.reddit_video.fallback_url && (
+						<video controls={true}>
+							<source
+								src={
+									data.secure_media.reddit_video.fallback_url
+								}
+								type='video/mp4'
+							/>
+						</video>
+					)}
 
 					<div className='SingleViewContent__content'>
 						{data.selftext}
@@ -75,20 +91,9 @@ export function Post (props) {
 			<PostComments post={data} />
 
 			<div className='SingleViewFooter'>
-				<Button variant='outlined'>
-					<MaterialIcon icon='reply' />
-					Reply
-				</Button>
-
-				<Button variant='outlined'>
-					<MaterialIcon icon='reply_all' />
-					Reply All
-				</Button>
-
-				<Button variant='outlined'>
-					<MaterialIcon icon='forward' />
-					Forward
-				</Button>
+				<EmailButton icon='reply' text='Reply' />
+				<EmailButton icon='reply_all' text='Reply All' />
+				<EmailButton icon='forward' text='Forward' />
 			</div>
 		</div>
 	);
